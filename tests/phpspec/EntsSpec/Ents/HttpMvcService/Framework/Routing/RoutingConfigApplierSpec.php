a10201284e27c1ce3b5006e2e4c7a28b5561caf3
Sam Burns <sam.burns.uk@gmail.com>
2015-11-18 14:11:00 +0000

    Proper test coverage for Routing Config Applier

--- tests/phpspec/EntsSpec/Ents/HttpMvcService/Framework/Routing/RoutingConfigApplierSpec.php
+++ tests/phpspec/EntsSpec/Ents/HttpMvcService/Framework/Routing/RoutingConfigApplierSpec.php
@@ -1,14 +1,34 @@
 <?php
-
 namespace EntsSpec\Ents\HttpMvcService\Framework\Routing;
 
+use Ents\HttpMvcService\Framework\Routing\Route;
 use PhpSpec\ObjectBehavior;
+use Pimple\Container;
 use Prophecy\Argument;
+use Slim\Slim as SlimApplication;
 
 class RoutingConfigApplierSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('Ents\HttpMvcService\Framework\Routing\RoutingConfigApplier');
+    }
+
+    function it_can_configure_application_with_route(
+        SlimApplication $slimApplication,
+        Route           $route,
+        Container       $container
+    ) {
+        // ARRANGE
+        $route->controllerServiceId()->willReturn('controller-service-id');
+        $route->actionMethodName()->willReturn('indexAction');
+        $route->httpVerb()->willReturn('GET');
+        $route->pathExpression()->willReturn('/person/:id');
+
+        // ACT
+        $this->configureApplicationWithRoute($slimApplication, $route, $container);
+
+        // ASSERT
+        $slimApplication->get('/person/:id', Argument::type('callable'))->shouldHaveBeenCalled();
     }
 }
